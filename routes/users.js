7fc65744fc6aeeaf88fb4fdcd3f67abee9d93f99
Saad Nauman <saadijutt007@gmail.com>
2023-05-08 23:18:54 +0500

    Removed date check

--- routes/users.js
+++ routes/users.js
@@ -49,14 +49,12 @@ router.get("/data", async (_, res) => {
     const users = await User.find({}, "employeeID name attendance");
 
     const newUsers = users.map((user) => {
-      if (user.attendance.length > 0) {
-        const { employeeID, name, attendance } = user;
-        const newAttendance = attendance.filter((item) => {
-          const { date, status, _id } = item;
-          return { date, status, _id };
-        });
-        return { employeeID, name, attendance: newAttendance };
-      } else return;
+      const { employeeID, name, attendance } = user;
+      const newAttendance = attendance.filter((item) => {
+        const { date, status, _id } = item;
+        return { date, status, _id };
+      });
+      return { employeeID, name, attendance: newAttendance };
     });
 
     res.json(newUsers);
