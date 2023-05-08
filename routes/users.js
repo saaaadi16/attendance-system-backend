3291bdb644af268dc59826bdb5a59888dcfc22cf
Saad Nauman <saadijutt007@gmail.com>
2023-05-08 22:49:56 +0500

    Added get users API

--- routes/users.js
+++ routes/users.js
@@ -1,46 +1,65 @@
-const express = require('express');
-const User = require('../models/user');
+const express = require("express");
+const User = require("../models/user");
 
 const router = express.Router();
 
 // User registration
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
   const { employeeID, name, faceData } = req.body;
 
   try {
     const newUser = new User({ employeeID, name, faceData });
     await newUser.save();
-    res.status(201).json({ message: 'User registered successfully' });
+    res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    res.status(400).json({ message: 'Error registering user', error });
+    res.status(400).json({ message: "Error registering user", error });
   }
 });
 
 // Get user face data
-router.get('/face-data', async (req, res) => {
+router.get("/face-data", async (req, res) => {
   try {
-    const users = await User.find({}, 'employeeID name faceData');
+    const users = await User.find({}, "employeeID name faceData");
     res.json(users);
   } catch (error) {
-    res.status(500).json({ message: 'Error fetching face data', error });
+    res.status(500).json({ message: "Error fetching face data", error });
   }
 });
 
 // Mark attendance
-router.post('/attendance', async (req, res) => {
+router.post("/attendance", async (req, res) => {
   const { employeeID, status } = req.body;
 
   try {
     const user = await User.findOne({ employeeID });
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
 
     user.attendance.push({ date: new Date(), status });
     await user.save();
-    res.status(201).json({ message: 'Attendance marked successfully' });
+    res.status(201).json({ message: "Attendance marked successfully" });
   } catch (error) {
-    res.status(500).json({ message: 'Error marking attendance', error });
+    res.status(500).json({ message: "Error marking attendance", error });
+  }
+});
+
+router.get("/user-data", async (_, res) => {
+  try {
+    const users = await User.find({}, "employeeID name attendance");
+
+    const newUsers = users.map((user) => {
+      const { employeeID, name, attendance } = user;
+      const newAttendance = attendance.map((item) => {
+        const { date, status } = item;
+        return { date, status };
+      });
+      return { employeeID, name, attendance: newAttendance };
+    });
+
+    res.json(newUsers);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching user data", error });
   }
 });
 
